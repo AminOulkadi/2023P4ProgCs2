3cdb352c8ea9718906799054e06d10bc8d1d32a8
Amin <34860@ma-web.nl>
2023-06-27 23:45:47 +0200

    les 9

---
+++ FileDirOpdracht/FileDirOpdracht/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        // Nieuwe lokale variabele "dir" van het type DirectoryInfo
+        DirectoryInfo dir = new DirectoryInfo(@"C:\Users\amino\OneDrive\Documenten\Ma (school)\bewijzenmap\1.4\progM4\2023P4ProgCs2");
+
+        // Bestanden ophalen
+        FileInfo[] files = dir.GetFiles();
+
+        Console.WriteLine("Bestanden:");
+        for (int i = 0; i < files.Length; i++)
+        {
+            Console.WriteLine(files[i].Name);
+        }
+
+        // Directories ophalen
+        DirectoryInfo[] directories = dir.GetDirectories();
+
+        Console.WriteLine("Directories:");
+        for (int i = 0; i < directories.Length; i++)
+        {
+            Console.WriteLine(directories[i].Name);
+        }
+    }
+}
