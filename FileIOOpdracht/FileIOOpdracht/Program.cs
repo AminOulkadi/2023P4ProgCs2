9430e87820646dac63534f210db171a808b1f7eb
Amin <34860@ma-web.nl>
2023-06-28 23:30:03 +0200

    les 10

---
+++ FileIOOpdracht/FileIOOpdracht/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] content;
+
+
+        string filePath = "leesdezefile.txt";
+
+
+        content = File.ReadAllLines(filePath);
+
+
+        for (int i = 0; i < content.Length; i++)
+        {
+            Console.WriteLine(content[i]);
+        }
+
+
+
+        Console.ReadLine();
+    }
+}
